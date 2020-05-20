37aece6665d5b9444b590555bf559ca271cddb3a
RodolfoSoares <rodolfobredoff@gmail.com>
2020-05-19 22:38:54 -0300

    Módulo de Localização instalado e configurado

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-05-20 01:18:56 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-05-20T01:18:56+00:00	INFO 172.18.0.1	update	Update started by user Super User (520). Old version is 3.9.16.
+2020-05-20T01:19:00+00:00	INFO 172.18.0.1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.18-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20200520%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200520T011853Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=b62be7be39ea43a9aa1a70d4da8437dcf45af1392da93d1a88f5f8626a3e4460.
+2020-05-20T01:19:22+00:00	INFO 172.18.0.1	update	File Joomla_3.9.18-Stable-Update_Package.zip downloaded.
+2020-05-20T01:19:23+00:00	INFO 172.18.0.1	update	Starting installation of new version.
+2020-05-20T01:27:11+00:00	INFO 172.18.0.1	update	Finalising installation.
+2020-05-20T01:27:11+00:00	INFO 172.18.0.1	update	Deleting removed files and folders.
+2020-05-20T01:27:19+00:00	INFO 172.18.0.1	update	Cleaning up after installation.
+2020-05-20T01:27:19+00:00	INFO 172.18.0.1	update	Update to version 3.9.18 is complete.
