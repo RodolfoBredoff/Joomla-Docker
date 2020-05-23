d9fe97aff2ed0567e02002045ee543489048f635
RodolfoSoares <rodolfobredoff@gmail.com>
2020-05-23 09:37:18 -0300

    Configuração de página de recados,página de cerimônia e icones de redes sociais

---
+++ administrator/logs/convertforms_errors.php
@@ -0,0 +1,111 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-05-22 16:42:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: message
+
+
+Identity
+---------------------------------------------------------------------------
+Date Time:          2020-05-22 16:42:54
+Error Category:     Submit
+Error message:      Error sending email: Não foi possível instanciar a função mail.
+Form:               Template: Lista de convidados (1)
+Session ID:         bac390476623b60d3ae89e3b32335e11
+IP Address:         172.18.0.1
+User Agent:         Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36
+Device:             desktop
+Logged In Username: 
+Logged In Name:     
+
+Data
+---------------------------------------------------------------------------
+Array
+(
+    [option] => com_convertforms
+    [task] => submit
+    [cf] => Array
+        (
+            [text_2] => rodolfo
+            [Email] => rodolfo@gmail.com
+            [radio_3] => Array
+                (
+                    [0] => Sim
+                )
+
+            [form_id] => 1
+            [hnpt] => 
+        )
+
+    [b8c6acbe716a179e70f5c1ad4405e9bf] => 1
+    [pma_lang] => pt_BR
+    [__utmz] => 111872281.1589281951.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)
+    [pmaUser-1] => {"iv":"FFX0C7614QoxKBbOdWmb\/g==","mac":"b26a7f099e2bc40e1d0a46b9a40e9c6856121d6d","payload":"E1ytBxt2RDg5Acd7rFiczA=="}
+    [__utmc] => 111872281
+    [6f7d519c02a8502c199d12a048e89e9f] => c8b9d51ef1aa4ddda95dfa188fafa556
+    [ConvertFormsState0] => fields-
+    [c54f06eb0be80316d3c8896069718eee] => bac390476623b60d3ae89e3b32335e11
+    [__utma] => 111872281.1747338670.1589281951.1590158717.1590165309.25
+    [__utmt] => 1
+    [ConvertFormsState1] => design-box
+    [__utmb] => 111872281.3.10.1590165309
+    [Itemid] => 
+)
+
+
+Request Headers
+---------------------------------------------------------------------------
+Array
+(
+    [HTTP_AUTHORIZATION] => 
+    [HTTP_HOST] => localhost:8080
+    [HTTP_CONNECTION] => keep-alive
+    [CONTENT_LENGTH] => 678
+    [HTTP_X_CSRF_TOKEN] => b8c6acbe716a179e70f5c1ad4405e9bf
+    [HTTP_X_REQUESTED_WITH] => XMLHttpRequest
+    [HTTP_X_AJAX_ENGINE] => Convert Forms!
+    [HTTP_USER_AGENT] => Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36
+    [CONTENT_TYPE] => multipart/form-data; boundary=----WebKitFormBoundaryzblo4EtH6CiI32vc
+    [HTTP_ACCEPT] => */*
+    [HTTP_ORIGIN] => http://localhost:8080
+    [HTTP_SEC_FETCH_SITE] => same-origin
+    [HTTP_SEC_FETCH_MODE] => cors
+    [HTTP_SEC_FETCH_DEST] => empty
+    [HTTP_REFERER] => http://localhost:8080/index.php/cerimonia/confirmar-presenca
+    [HTTP_ACCEPT_ENCODING] => gzip, deflate, br
+    [HTTP_ACCEPT_LANGUAGE] => pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7
+    [HTTP_COOKIE] => pma_lang=pt_BR; __utmz=111872281.1589281951.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); pmaUser-1=%7B%22iv%22%3A%22FFX0C7614QoxKBbOdWmb%5C%2Fg%3D%3D%22%2C%22mac%22%3A%22b26a7f099e2bc40e1d0a46b9a40e9c6856121d6d%22%2C%22payload%22%3A%22E1ytBxt2RDg5Acd7rFiczA%3D%3D%22%7D; __utmc=111872281; 6f7d519c02a8502c199d12a048e89e9f=c8b9d51ef1aa4ddda95dfa188fafa556; ConvertFormsState0=fields-; c54f06eb0be80316d3c8896069718eee=bac390476623b60d3ae89e3b32335e11; __utma=111872281.1747338670.1589281951.1590158717.1590165309.25; __utmt=1; ConvertFormsState1=design-box; __utmb=111872281.3.10.1590165309
+    [PATH] => /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+    [SERVER_SIGNATURE] => Apache/2.4.38 (Debian) Server at localhost Port 8080
+
+    [SERVER_SOFTWARE] => Apache/2.4.38 (Debian)
+    [SERVER_NAME] => localhost
+    [SERVER_ADDR] => 172.18.0.2
+    [SERVER_PORT] => 8080
+    [REMOTE_ADDR] => 172.18.0.1
+    [DOCUMENT_ROOT] => /var/www/html
+    [REQUEST_SCHEME] => http
+    [CONTEXT_PREFIX] => 
+    [CONTEXT_DOCUMENT_ROOT] => /var/www/html
+    [SERVER_ADMIN] => webmaster@localhost
+    [SCRIPT_FILENAME] => /var/www/html/index.php
+    [REMOTE_PORT] => 43186
+    [GATEWAY_INTERFACE] => CGI/1.1
+    [SERVER_PROTOCOL] => HTTP/1.1
+    [REQUEST_METHOD] => POST
+    [QUERY_STRING] => option=com_convertforms&task=submit
+    [REQUEST_URI] => /?option=com_convertforms&task=submit
+    [SCRIPT_NAME] => /index.php
+    [PHP_SELF] => /index.php
+    [REQUEST_TIME_FLOAT] => 1590165773.45
+    [REQUEST_TIME] => 1590165773
+    [argv] => Array
+        (
+            [0] => option=com_convertforms&task=submit
+        )
+
+    [argc] => 1
+)
+
+
---
+++ administrator/logs/plg_system_nrframework.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-05-22 16:42:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-05-22T16:42:54+00:00	DEBUG 172.18.0.1	nrframework	Não foi possível instanciar a função mail.
